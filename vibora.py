49d2432717a983bd5fbcc11b66875c5be90064f0
melissamtzm <a00827008@itesm.mx>
2020-09-15 12:03:29 -0500

    Movimiento comida

--- vibora.py
+++ vibora.py
@@ -36,11 +36,34 @@ def move():
         square(head.x, head.y, 9, 'red')
         update()
         return
-
+    
+    #Se define una variable booleana para controlar que la comida no tope con los límites
+    limite = True
+    #Para cada paso que dé la serpiente, la comida se moverá un paso
+    if head != food:
+        #El ciclo while se ejecutará hasta que se genere una posición que no tope con el límite correspondiente
+        while limite == True: 
+            #Se genera un número random para representar la dirección a la que se moverá la comida
+            direccion = randrange(1,5) #1 arriba, 2 derecha, 3 izquierda, 4 abajo
+            #Dependiendo de la dirección la coordenada correspondiente de la comida se moverá (10 posiciones para que la serpiente pueda coincidir) 
+            if direccion == 1 and food.y < 190:
+                food.y += 10
+                limite = False
+            elif direccion == 2 and food.x < 190:
+                food.x += 10
+                limite = False
+            elif direccion == 3 and food.x > -200:
+                food.x -= 10
+                limite = False
+            elif direccion == 4 and food.y > -200:
+                food.y -= 10
+                limite = False
+            
     snake.append(head)
 
-    if head == food: #Si la cabeza llega a la posición de la comida, la comida se cambia de lugar
-        print('Snake:', len(snake)) 
+    if head == food: 
+        print('Snake:', len(snake)) #Imprime la longitud de la serpiente
+        #Si la cabeza llega a la posición de la comida, la comida se cambia de lugar
         food.x = randrange(-15, 15) * 10
         food.y = randrange(-15, 15) * 10
     else:
