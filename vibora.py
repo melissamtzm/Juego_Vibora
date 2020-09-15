a25dd99aadd7eca3a905dc6ebbceb3483bd3165b
A01720572 <71277782+A01720572@users.noreply.github.com>
2020-09-15 10:36:35 -0500

    Documentacion.py

--- vibora.py
+++ vibora.py
@@ -13,33 +13,34 @@ from turtle import *
 from random import randrange
 from freegames import square, vector
 
-food = vector(0, 0)
+#Se crean vectores para las posiciones de los elementos del juego
+food = vector(0, 0) 
 snake = [vector(10, 0)]
 aim = vector(0, -10)
 
 def change(x, y):
-    "Change snake direction."
+    "Change snake direction." #Cambia las coordenadas x y y de la dirección
     aim.x = x
     aim.y = y
 
 def inside(head):
-    "Return True if head inside boundaries."
+    "Return True if head inside boundaries." #Verifica que la serpiente no haya topado con el límite
     return -200 < head.x < 190 and -200 < head.y < 190
 
 def move():
-    "Move snake forward one segment."
+    "Move snake forward one segment." #Dependiendo de la dirección se mueve la serpiente
     head = snake[-1].copy()
     head.move(aim)
 
-    if not inside(head) or head in snake:
+    if not inside(head) or head in snake: #Si topa con el límite o con ella mísma, la cabeza de la serpiente se hace roja
         square(head.x, head.y, 9, 'red')
         update()
         return
 
     snake.append(head)
 
-    if head == food:
-        print('Snake:', len(snake))
+    if head == food: #Si la cabeza llega a la posición de la comida, la comida se cambia de lugar
+        print('Snake:', len(snake)) 
         food.x = randrange(-15, 15) * 10
         food.y = randrange(-15, 15) * 10
     else:
@@ -47,17 +48,18 @@ def move():
 
     clear()
 
-    for body in snake:
+    for body in snake: #Color de la serpiente
         square(body.x, body.y, 9, 'black')
 
-    square(food.x, food.y, 9, 'green')
+    square(food.x, food.y, 9, 'green') #Color de la comida
     update()
     ontimer(move, 100)
 
-setup(420, 420, 370, 0)
+setup(420, 420, 370, 0) #límites del juego
 hideturtle()
 tracer(False)
 listen()
+#Comandos para mover la serpiente
 onkey(lambda: change(10, 0), 'Right')
 onkey(lambda: change(-10, 0), 'Left')
 onkey(lambda: change(0, 10), 'Up')
